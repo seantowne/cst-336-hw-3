331b34e9a3850bc557fd4261dc463c91239cfbeb
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-09 04:40:41 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ $("#lookupSearchTerm").on('click', function() {
     var accessKey = "Q8PqdylVXR-3P-qyQZJaAupVTPp1iZ8rKsbtyvymtWo";
     var secretKey = "eXXSn6wxlkLIkAy-cctk0SLLAWleQqC9wwa9DL5tKeo";
     var urlBase = "https://api.unsplash.com/search/collections?page=1&query=";
-    var searchTerm = "dogs";
+    //var searchTerm = "dogs";
     urlBase += searchTerm + "&client_id=" + accessKey;
     
     $.ajax({
