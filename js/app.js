07eec43179f6ee87aaa6f972162ebb4bc5c692c4
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-09 04:20:58 +0000

    AutomatedCommitAndPush

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 
-var weatherApiKey = "ff4f1b59e21d4048c11ff586ffe76523";
-var locationApiKey = "yshNbMqn0rYVh5lIV0154ZoxUfq5hUNQgy6BKpwb5snh2AzyMEXCOICfbHjLg4e5";
+var accessKey = "Q8PqdylVXR-3P-qyQZJaAupVTPp1iZ8rKsbtyvymtWo";
+var secretKey = "eXXSn6wxlkLIkAy-cctk0SLLAWleQqC9wwa9DL5tKeo";
 
-var locationApiUrlBase = "https://www.zipcodeapi.com/rest/"+locationApiKey+"/info.json/<zip_code>/degrees"
+// https://api.unsplash.com/search/collections?page=1&query=office&client_id=Q8PqdylVXR-3P-qyQZJaAupVTPp1iZ8rKsbtyvymtWo
+var urlBase = "https://api.unsplash.com/search/collections?page=1&query=";
+var searchTerm = "dogs";
+urlBase += searchTerm + "&client_id=" + accessKey;
 
-var weatherUrlBase = "api.openweathermap.org/data/2.5/weather"
-// https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22
-
-var locationGet = locationApiUrlBase.replace("<zip_code>", '93933');
-console.log(locationGet);
 $.ajax({
-    url: locationGet,
+    method: "GET",
+    url: urlBase,
     dataType: "json",
     success: function(result, status){
         alert("success");
@@ -22,26 +21,3 @@ $.ajax({
     }
 });
 
-/*
-
-$.ajax({
-        method: "GET",
-        url: url,
-        dataType: "json",
-        success: function(result, status){
-            //alert("success");
-            console.log(result);
-            console.log(result.message[0]);
-            for ( var i in result.message ){
-                console.log(result.message[i]);
-                var img = "<img class='img-thumbnail' src='"+result.message[i]+"'>";
-                var div = "<div class='flex-item'>"+img+"</div>";
-                $("#doggos").append(div);
-            }
-        },
-        error: function(status, error){
-            alert("failure");
-        }
-    });
-
-*/
